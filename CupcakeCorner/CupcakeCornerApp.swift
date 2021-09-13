1325ffdf4d83a8dd40372944cb1e78a6920a87e3
Vincent DELMAESTRO <vincent.delmaestro@yahoo.com>
2021-09-13 18:44:58 +0200

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Vincent Delmaestro on 13/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
